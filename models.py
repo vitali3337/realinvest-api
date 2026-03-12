f8701be1533b0521aa4a43e207228de7df3cd8b0
vitalii kostuk <36382856+vitali3337@users.noreply.github.com>
2026-03-13 00:17:38 +0200

    Update models.py
    from sqlalchemy import Column, Integer, String
    from database import Base
    
    class Listing(Base):
        __tablename__ = "listings"
    
        id = Column(Integer, primary_key=True, index=True)
    
        title = Column(String)
        price = Column(Integer)
        city = Column(String)
        rooms = Column(String)
        floor = Column(String)
        phone = Column(String)
        description = Column(String)
        image = Column(String)

--- models.py
+++ models.py
@@ -1,8 +1,7 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String
 from database import Base
 
 class Listing(Base):
-
     __tablename__ = "listings"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -10,21 +9,8 @@ class Listing(Base):
     title = Column(String)
     price = Column(Integer)
     city = Column(String)
-
-    rooms = Column(Integer)
+    rooms = Column(String)
     floor = Column(String)
-
     phone = Column(String)
-    description = Column(Text)
-
+    description = Column(String)
     image = Column(String)
-
-    image2 = Column(String)
-    image3 = Column(String)
-    image4 = Column(String)
-    image5 = Column(String)
-    image6 = Column(String)
-    image7 = Column(String)
-    image8 = Column(String)
-    image9 = Column(String)
-    image10 = Column(String)
