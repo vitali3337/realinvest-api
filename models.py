9fbc794259311623b90dbc0a6967a6ed77aa6958
vitalii kostuk <36382856+vitali3337@users.noreply.github.com>
2026-03-12 23:46:16 +0200

    Update models.py
    from sqlalchemy import Column, Integer, String, Text
    from database import Base
    
    class Listing(Base):
    
        __tablename__ = "listings"
    
        id = Column(Integer, primary_key=True, index=True)
    
        title = Column(String)
        price = Column(Integer)
        city = Column(String)
    
        rooms = Column(Integer)
        floor = Column(String)
    
        phone = Column(String)
        description = Column(Text)
    
        image = Column(String)
    
        image2 = Column(String)
        image3 = Column(String)
        image4 = Column(String)
        image5 = Column(String)
        image6 = Column(String)
        image7 = Column(String)
        image8 = Column(String)
        image9 = Column(String)
        image10 = Column(String)

--- models.py
+++ models.py
@@ -1,17 +1,30 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
+from sqlalchemy import Column, Integer, String, Text
+from database import Base
 
 class Listing(Base):
+
     __tablename__ = "listings"
 
     id = Column(Integer, primary_key=True, index=True)
+
     title = Column(String)
     price = Column(Integer)
     city = Column(String)
+
     rooms = Column(Integer)
     floor = Column(String)
+
     phone = Column(String)
-    description = Column(String)
+    description = Column(Text)
+
     image = Column(String)
+
+    image2 = Column(String)
+    image3 = Column(String)
+    image4 = Column(String)
+    image5 = Column(String)
+    image6 = Column(String)
+    image7 = Column(String)
+    image8 = Column(String)
+    image9 = Column(String)
+    image10 = Column(String)
